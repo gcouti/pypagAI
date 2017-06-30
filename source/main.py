88d85af889742ccbdd9d2191480ddbe1a567914c
gcouti <gcouti@gmail.com>
2017-06-29 23:46:40 -0300

    First commit
    Agents to solve bAbi questions

---
+++ source/main.py
@@ -0,0 +1,217 @@
+from __future__ import print_function
+
+import csv
+import os
+
+# Comment this if tensor flow crash
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+
+import sys
+import time
+
+import copy
+
+from parlai.agents.ir_baseline.ir_baseline import IrBaselineAgent
+from parlai.core.dict import DictionaryAgent
+from parlai.core.worlds import create_task
+from parlai.core.params import ParlaiParser
+from parlai.agents.remote_agent.remote_agent import ParsedRemoteAgent
+from my_agents import N2NMemAgent, DummyAgent, RNAgent
+
+
+"""
+    This is the main file 
+     
+    Here you can run our experiments. To run just type 
+    
+    python main.py  
+ 
+    This script use ParlAI Framework to do the experimentation process
+          
+          
+    References:
+    
+        ParlAI Framework
+            - https://github.com/facebookresearch/ParlAI#requirements
+
+        LUA Implementation n2n memory
+            - https://github.com/facebook/MemNN
+                    
+        Kera FB-LSTM | LSTM | GRU  | IRNN | RNN
+            - https://gist.github.com/Smerity/418a4e7f9e719ff02bf3
+            - https://github.com/Smerity/keras_qa/blob/master/qa.py
+            - https://gist.github.com/SNagappan/a7be6ce6e75c36c7406e
+            
+        Python N2N Mem
+            - https://github.com/fchollet/keras/blob/master/examples/babi_memnn.py
+            - https://github.com/vinhkhuc/MemN2N-babi-python
+            - https://github.com/domluna/memn2n/blob/master/memn2n/memn2n.py
+
+        Seq2Seq Network
+            - pip install http://download.pytorch.org/whl/cu80/torch-0.1.12.post2-cp35-cp35m-linux_x86_64.whl
+            - pip install torchvision
+
+        DNC
+            - https://github.com/FrownyFace/DNC/blob/master/dnc.py
+            - https://github.com/brendanator/differentiable-neural-computer
+            - https://github.com/Mostafa-Samir/DNC-tensorflow/tree/master/dnc
+            - https://github.com/bgavran/DNC
+            - https://github.com/deepmind/dnc/blob/master/train.py
+        RN
+            - https://github.com/kimhc6028/relational-networks/blob/master/model.py
+"""
+
+
+def create_dictionary(argparser):
+    print('Setting up dictionary.')
+    # set up dictionary
+    DictionaryAgent.add_cmdline_args(argparser)
+    opt = argparser.parse_args()
+    dictionary = DictionaryAgent(opt)
+
+    if not opt.get('dict_loadpath'):
+        # build dictionary since we didn't load it
+        ordered_opt = copy.deepcopy(opt)
+        for datatype in ['train:ordered', 'valid']:
+            # we use train and valid sets to build dictionary
+            ordered_opt['datatype'] = datatype
+            ordered_opt['numthreads'] = 1
+            world_dict = create_task(ordered_opt, dictionary)
+
+            print('Dictionary building on {} data.'.format(datatype))
+            cnt = 0
+            # pass examples to dictionary
+            for _ in world_dict:
+                cnt += 1
+                if cnt > opt['dict_max_exs'] and opt['dict_max_exs'] > 0:
+                    print('Processed {} exs, moving on.'.format(
+                        opt['dict_max_exs']))
+                    # don't wait too long...
+                    break
+
+                world_dict.parley()
+
+        # we need to save the dictionary to load it in memnn (sort it by freq)
+        dictionary.save(opt['dict_file'], sort=True)
+
+        print('Dictionary ready, moving on to training.')
+
+        return dictionary, opt
+
+
+if __name__ == "__main__":
+
+    print("Start experiments")
+    # Get command line arguments
+    argparser = ParlaiParser()
+    argparser.add_argument('--model', default='baseline', type=str)
+    argparser.add_argument('--num-examples', default=1000, type=int)
+    argparser.add_argument('--num-its', default=100, type=int)
+    argparser.add_argument('--dict-max-exs', default=1000, type=int)
+
+    opt ={}
+    opt['model'] = 'baseline'
+
+    if opt['model'] == 'baseline':
+        print('Baseline Model')
+        IrBaselineAgent.add_cmdline_args(argparser)
+
+        opt = argparser.parse_args()
+        agent = IrBaselineAgent(opt)
+
+    elif opt['model'] == 'memnn':
+        print('MemoryNN Model')
+        ParsedRemoteAgent.add_cmdline_args(argparser)
+        dictionary, opt = create_dictionary(argparser)
+        parlai_home = os.environ['PARLAI_HOME']
+        if '--remote-cmd' not in sys.argv:
+            if os.system('which luajit') != 0:
+                raise RuntimeError('Could not detect torch luajit installed: ' +
+                                   'please install torch from http://torch.ch ' +
+                                   'or manually set --remote-cmd for this example.')
+            sys.argv.append('--remote-cmd')
+            sys.argv.append('luajit {}/parlai/agents/'.format(parlai_home) +
+                            'memnn_luatorch_cpu/memnn_zmq_parsed.lua')
+
+        if '--remote-args' not in sys.argv:
+            sys.argv.append('--remote-args')
+            sys.argv.append('{}/examples/'.format(parlai_home) +
+                            'memnn_luatorch_cpu/params_default.lua')
+
+        agent = ParsedRemoteAgent(opt, {'dictionary': dictionary})
+
+    elif opt['model'] == 'n2nmem':
+
+        print('N2N Mem Model')
+        N2NMemAgent.add_cmdline_args(argparser)
+        dictionary, opt = create_dictionary(argparser)
+        opt = argparser.parse_args()
+        agent = N2NMemAgent(opt)
+
+    elif opt['model'] == 'rn':
+
+        print('rn Mem Model')
+        RNAgent.add_cmdline_args(argparser)
+        dictionary, opt = create_dictionary(argparser)
+        opt = argparser.parse_args()
+        agent = RNAgent(opt)
+
+    elif opt['model'] == 'dummy':
+        print('Dummy Model')
+        DummyAgent.add_cmdline_args(argparser)
+        dictionary, opt = create_dictionary(argparser)
+        opt = argparser.parse_args()
+        agent = DummyAgent(opt)
+    else:
+        print('Baseline Model')
+        IrBaselineAgent.add_cmdline_args(argparser)
+        dictionary, opt = create_dictionary(argparser)
+        opt = argparser.parse_args()
+        agent = IrBaselineAgent(opt)
+
+    opt['datatype'] = 'train'
+    world_train = create_task(opt, agent)
+
+    opt['datatype'] = 'valid'
+    world_valid = create_task(opt, agent)
+
+    start = time.time()
+    results = []
+
+    model_name = opt['model']
+    task_name = opt['task']
+
+    with world_train:
+
+        iteration = 0
+        for _ in range(opt['num_its']):
+            print('[ training ] %i' % iteration)
+            for _ in range(opt['num_examples'] * opt.get('numthreads', 1)):
+                world_train.parley()
+
+            world_train.synchronize()
+
+            world_valid.reset()
+            for _ in world_valid:  # check valid accuracy
+                world_valid.parley()
+
+            report_valid = world_valid.report()
+            report_valid['TASK'] = task_name
+            report_valid['ITER'] = iteration
+            results.append(report_valid)
+
+            iteration += 1
+
+        # show some example dialogs after training:
+        world_valid = create_task(opt, agent)
+        for _k in range(10):
+            world_valid.parley()
+            print(world_valid.display())
+
+    print('finished in {} s'.format(round(time.time() - start, 2)))
+
+    with open('result_%s_%s_.csv' % (model_name, task_name), 'w') as file:
+        writer = csv.writer(file)
+        for row in results:
+            writer.writerow([model_name, row['TASK'], row['ITER'], row['accuracy'], row['hits@k'][5], row['hits@k'][10]])
---
+++ source/my_agents.py
@@ -0,0 +1,412 @@
+import copy
+
+from keras.optimizers import SGD
+from parlai.core.dict import DictionaryAgent
+from parlai.core.agents import Agent
+
+from keras.models import Sequential, Model
+from keras.layers.embeddings import Embedding
+from keras.layers import Input, Activation, Dense, Permute, Dropout, add, dot, concatenate, BatchNormalization, \
+    Bidirectional
+from keras.layers import LSTM, GRU
+
+from keras.models import Sequential, Model
+from keras.layers import Dense, Dropout, Activation, Flatten, Input, Embedding, \
+    LSTM, Bidirectional, Lambda, Concatenate, Add, SimpleRNN
+from keras.layers.convolutional import Conv2D, MaxPooling2D, AveragePooling2D
+from keras.layers.normalization import BatchNormalization, regularizers
+from keras.optimizers import Adam, RMSprop
+from keras import backend as K
+
+import tensorflow as tf
+
+import collections
+import numpy as np
+import os
+
+from tensorflow.python.ops import rnn_cell_impl
+from tensorflow.python.ops.control_flow_ops import with_dependencies
+
+# import torch
+# import torch.nn as nn
+# import torch.nn.functional as F
+# import torch.optim as optim
+
+from torch.autograd import Variable
+
+
+class DummyAgent(Agent):
+    """
+        This Agent retrieve only the first candidates
+    """
+    @staticmethod
+    def add_cmdline_args(parser):
+        DictionaryAgent.add_cmdline_args(parser)
+
+    def __init__(self, opt, shared=None):
+        super().__init__(opt)
+        self.id = 'DummyAgent'
+        self.dictionary = DictionaryAgent(opt)
+        self.opt = opt
+
+    def observe(self, obs):
+        self.observation = obs
+        self.dictionary.observe(obs)
+        return obs
+
+    def act(self):
+        if self.opt.get('datatype', '').startswith('train'):
+            self.dictionary.act()
+
+        obs = self.observation
+        reply = {}
+        reply['id'] = self.getID()
+
+        # Rank candidates
+        if 'label_candidates' in obs and len(obs['label_candidates']) > 0:
+            reply['text_candidates'] = self._rank_candidates(obs)
+            reply['text'] = reply['text_candidates'][0]
+        else:
+            reply['text'] = "I don't know."
+        return reply
+
+    @staticmethod
+    def _rank_candidates(obs):
+        return list(obs['label_candidates'])
+
+    def save(self, fname):
+        self.dictionary.save(fname + '.dict')
+
+    def load(self, fname):
+        self.dictionary.load(fname + '.dict')
+
+
+class N2NMemAgent(Agent):
+    """
+    Python + Keras implementation of End-to-end Memory Network
+
+    References:
+        - Jason Weston, Antoine Bordes, Sumit Chopra, Tomas Mikolov, Alexander M. Rush,
+        "Towards AI-Complete Question Answering: A Set of Prerequisite Toy Tasks",
+        http://arxiv.org/abs/1502.05698
+
+        - Sainbayar Sukhbaatar, Arthur Szlam, Jason Weston, Rob Fergus,
+        "End-To-End Memory Networks",
+        http://arxiv.org/abs/1503.08895
+
+        Reaches 98.6% accuracy on task 'single_supporting_fact_10k' after 120 epochs.
+        Time per epoch: 3s on CPU (core i7).
+    """
+    @staticmethod
+    def add_cmdline_args(parser):
+        parser.add_argument('-lp', '--length_penalty', default=0.5, help='length penalty for responses')
+
+    def __init__(self, opt, shared=None):
+        super().__init__(opt)
+        self.id = 'N2NMemAgent'
+
+        self._dictionary = DictionaryAgent(opt)
+        self.opt = opt
+        self.episode_done = True
+
+        self._create_network(opt)
+
+    def _create_network(self, opt):
+
+        statement_size = 8
+        self._vocab_size = len(self._dictionary)
+        self._story_maxlen = 15 * statement_size
+        self._query_maxlen = 1 * statement_size
+        drop_out = 0.3
+        activation = 'softmax'
+        samples = 32
+        embedding = 64
+
+        # placeholders
+        input_sequence = Input((self._story_maxlen,))
+        question = Input((self._query_maxlen,))
+
+        # encoders
+        # embed the input sequence into a sequence of vectors
+        input_encoder_m = Sequential()
+        input_encoder_m.add(Embedding(input_dim=self._vocab_size, output_dim=embedding))
+        input_encoder_m.add(Dropout(drop_out))
+        # output: (samples, story_maxlen, embedding_dim)
+
+        # embed the input into a sequence of vectors of size query_maxlen
+        input_encoder_c = Sequential()
+        input_encoder_c.add(Embedding(input_dim=self._vocab_size, output_dim=self._query_maxlen))
+        input_encoder_c.add(Dropout(drop_out))
+        # output: (samples, story_maxlen, query_maxlen)
+
+        # embed the question into a sequence of vectors
+        question_encoder = Sequential()
+        question_encoder.add(Embedding(input_dim=self._vocab_size, output_dim=embedding, input_length=self._query_maxlen))
+        question_encoder.add(Dropout(drop_out))
+        # output: (samples, query_maxlen, embedding_dim)
+
+        # encode input sequence and questions (which are indices)
+        # to sequences of dense vectors
+        input_encoded_m = input_encoder_m(input_sequence)
+        input_encoded_c = input_encoder_c(input_sequence)
+        question_encoded = question_encoder(question)
+
+        # compute a 'match' between the first input vector sequence
+        # and the question vector sequence
+        # shape: `(samples, story_maxlen, query_maxlen)`
+        match = dot([input_encoded_m, question_encoded], axes=(2, 2))
+        match = Activation(activation)(match)
+
+        # add the match matrix with the second input vector sequence
+        response = add([match, input_encoded_c])  # (samples, story_maxlen, query_maxlen)
+        response = Permute((2, 1))(response)  # (samples, query_maxlen, story_maxlen)
+
+        # concatenate the match matrix with the question vector sequence
+        answer = concatenate([response, question_encoded])
+
+        # the original paper uses a matrix multiplication for this reduction step.
+        # we choose to use a RNN instead.
+        answer = SimpleRNN(samples)(answer)  # (samples, 32)
+
+        # one regularization layer -- more would probably be needed.
+        answer = Dropout(drop_out)(answer)
+        answer = Dense(self._vocab_size)(answer)  # (samples, vocab_size)
+        # we output a probability distribution over the vocabulary
+        answer = Activation(activation)(answer)
+
+        # build the final model
+        self._model = Model([input_sequence, question], answer)
+        self._model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
+
+    def observe(self, obs):
+        observation = copy.deepcopy(obs)
+        if not self.episode_done:
+            # if the last example wasn't the end of an episode, then we need to
+            # recall what was said in that example
+            prev_dialogue = self.observation['text']
+            observation['text'] = prev_dialogue + '\n' + observation['text']
+        self.observation = observation
+        self.episode_done = observation['episode_done']
+        return observation
+
+    def act(self):
+        if self.opt.get('datatype', '').startswith('train'):
+            self._dictionary.act()
+
+        obs = self.observation
+        reply = {}
+        reply['id'] = self.getID()
+
+        # Rank candidates
+        if 'label_candidates' in obs and len(obs['label_candidates']) > 0:
+            reply['text_candidates'] = self._rank_candidates(obs)
+            reply['text'] = reply['text_candidates'][0]
+        else:
+            reply['text'] = "I don't know."
+        return reply
+
+    def _rank_candidates(self, obs):
+
+        history = obs['text'].split('\n')
+        inputs_train = np.array([0] * self._story_maxlen)
+        history_vec = self._dictionary.txt2vec("\n".join(history[:len(history) - 1]))
+        inputs_train[len(inputs_train) - len(history_vec):] = history_vec
+
+        queries_train = np.array([0] * self._query_maxlen)
+        query_vec = self._dictionary.txt2vec(history[len(history) - 1])
+        queries_train[len(queries_train) - len(query_vec):] = query_vec
+
+        inputs_train = np.array([inputs_train])
+        queries_train = np.array([queries_train])
+
+        if 'labels' in obs:
+            answers_train = [0.] * self._vocab_size
+            answers_train[self._dictionary.txt2vec(obs['labels'][0])[0]] = 1
+
+            # train
+            self._model.fit([inputs_train, queries_train], np.array([answers_train]),  verbose=False, epochs=1)
+
+            return [self._dictionary[np.argmax(answers_train)]]
+        else:
+            predicted = self._model.predict([inputs_train, queries_train], verbose=False)
+            return [self._dictionary[int(index)] for index in np.argsort(predicted[0])[::-1][:5]]
+
+    def save(self, fname):
+        self._dictionary.save(fname + '.dict')
+
+    def load(self, fname):
+        self._dictionary.load(fname + '.dict')
+
+    def reset(self):
+        super().reset()
+        self.episode_done = True
+
+
+class RNAgent(Agent):
+
+    @staticmethod
+    def add_cmdline_args(parser):
+        # DictionaryAgent.add_cmdline_args(parser)
+        parser.add_argument('-lp', '--length_penalty', default=0.5, help='length penalty for responses')
+        # parser.add_argument('--batch-size', type=int, default=64, metavar='N', help='input batch size for training (default: 64)')
+        # parser.add_argument('--epochs', type=int, default=20, metavar='N', help='number of epochs to train (default: 20)')
+        # parser.add_argument('--lr', type=float, default=0.0001, metavar='LR', help='learning rate (default: 0.0001)')
+        # parser.add_argument('--no-cuda', action='store_true', default=True, help='disables CUDA training')
+        # parser.add_argument('--seed', type=int, default=1, metavar='S', help='random seed (default: 1)')
+        # parser.add_argument('--log-interval', type=int, default=10, metavar='N', help='how many batches to wait before logging training status')
+        # parser.add_argument('--resume', type=str, help='resume from model stored')
+
+    def __init__(self, opt, shared=None):
+        super().__init__(opt)
+        self.id = 'DNCAgent'
+        self._dictionary = DictionaryAgent(opt)
+        self.opt = opt
+        self.episode_done = True
+
+        self._create_network(opt)
+
+    def _create_network(self, opt):
+        statement_size = 8
+        self._vocab_size = len(self._dictionary)
+        self._story_max_len = 15 * statement_size
+        self._query_max_len = 15 * statement_size
+
+        self._model = RN(opt)
+
+    def save(self, fname):
+        self._dictionary.save(fname + '.dict')
+
+    def load(self, fname):
+        self._dictionary.load(fname + '.dict')
+
+    def reset(self):
+        super().reset()
+        self._episode_done = True
+
+    def observe(self, obs):
+        observation = copy.deepcopy(obs)
+        if not self.episode_done:
+            # if the last example wasn't the end of an episode, then we need to
+            # recall what was said in that example
+            prev_dialogue = self.observation['text']
+            observation['text'] = prev_dialogue + '\n' + observation['text']
+        self.observation = observation
+        self.episode_done = observation['episode_done']
+        return observation
+
+    def act(self):
+        if self.opt.get('datatype', '').startswith('train'):
+            self._dictionary.act()
+
+        obs = self.observation
+        reply = {}
+        reply['id'] = self.getID()
+
+        # Rank candidates
+        if 'label_candidates' in obs and len(obs['label_candidates']) > 0:
+            reply['text_candidates'] = self._rank_candidates(obs)
+            reply['text'] = reply['text_candidates'][0]
+        else:
+            reply['text'] = "I don't know."
+        return reply
+
+    def _rank_candidates(self, obs):
+
+        history = obs['text'].split('\n')
+        inputs_train = np.array([0] * self._story_max_len)
+        history_vec = self._dictionary.txt2vec("\n".join(history[:len(history) - 1]))
+        inputs_train[len(inputs_train) - len(history_vec):] = history_vec
+
+        queries_train = np.array([0] * self._query_max_len)
+        query_vec = self._dictionary.txt2vec(history[len(history) - 1])
+        queries_train[len(queries_train) - len(query_vec):] = query_vec
+
+        inputs_train = np.array([inputs_train])
+        queries_train = np.array([queries_train])
+
+        inputs_train = np.reshape(inputs_train, (len(inputs_train), self._story_max_len, 1))
+        queries_train = np.reshape(queries_train, (len(queries_train), self._query_max_len, 1))
+
+        if 'labels' in obs:
+            answers_train = np.array([0.] * self._story_max_len)
+            answers_train[self._dictionary.txt2vec(obs['labels'][0])[0]] = 1
+            answers_train = np.array([answers_train])
+
+            predicted = self._model.train(inputs_train, queries_train, answers_train)
+            return [self._dictionary[int(index)] for index in np.argsort(predicted[0])[::-1][:5]]
+
+        else:
+            predicted = self._model.predict(inputs_train, queries_train)
+            return [self._dictionary[int(index)] for index in np.argsort(predicted[0])[::-1][:5]]
+
+
+class RN:
+
+    def __init__(self, args):
+
+        statement_size = 8
+        self._vocab_size = 15 * statement_size
+        self._story_maxlen = 15 * statement_size
+        self._query_maxlen = 15 * statement_size
+        drop_out = 0.5
+        embedding = 32
+        hidden = 256
+        lstm_units = 128
+
+        # placeholders
+        story = Input((self._story_maxlen,1))
+        question = Input((self._query_maxlen,1))
+
+        # encoders
+        # embed the input sequence into a sequence of vectors
+        # story_encoder = Sequential()
+        # story_encoder.add(Embedding(input_dim=self._vocab_size, output_dim=embedding))
+        # story_encoder.add(Dropout(drop_out))
+        # output: (samples, story_maxlen, embedding_dim)
+
+        # # embed the question into a sequence of vectors
+        # question_encoder = Sequential()
+        # question_encoder.add(Embedding(input_dim=self._vocab_size, output_dim=embedding, input_length=self._query_maxlen))
+        # question_encoder.add(Dropout(drop_out))
+        # output: (samples, query_maxlen, embedding_dim)
+
+        # Encode
+        # story_encoded = story_encoder(story)
+        # question_encoded = question_encoder(question)
+
+        story_encoded = LSTM(lstm_units)(story)
+        question_encoded = LSTM(lstm_units)(question)
+
+        response = concatenate([story_encoded, question_encoded])
+
+        # # add the match matrix with the second input vector sequence
+        g_response = Dense(hidden, activation='relu', input_dim=lstm_units)(response)
+        g_response = Dropout(drop_out)(g_response)
+        g_response = Dense(hidden, activation='relu')(g_response)
+        g_response = Dropout(drop_out)(g_response)
+        g_response = Dense(hidden, activation='relu')(g_response)
+        g_response = Dropout(drop_out)(g_response)
+        g_response = Dense(hidden, activation='relu')(g_response)
+
+        f_response = Dense(hidden, activation='relu', input_dim=hidden)(g_response)
+        f_response = Dropout(drop_out)(f_response)
+        f_response = Dense(hidden, activation='relu')(f_response)
+        f_response = Dropout(drop_out)(f_response)
+        f_response = Dense(hidden, activation='relu')(f_response)
+        f_response = Dropout(drop_out)(f_response)
+
+        pred = Dense(self._vocab_size, activation='softmax')(f_response)
+
+        self.model = Model(inputs=[story, question], outputs=pred)
+
+        optimizer = SGD(lr=0.01, decay=1e-6, momentum=0.9, nesterov=True)
+        self.model.compile(optimizer=optimizer, loss='binary_crossentropy', metrics=['accuracy'])
+
+    def train(self, story, question, answer):
+        prediction = self.model.predict([story, question], verbose=False)
+        self.model.fit([story, question], answer, verbose=False)
+        return prediction
+
+    def predict(self, story, question):
+        return self.model.predict([story, question], verbose=False)
+
