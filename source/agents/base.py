4f610092aad6bf0305bd242432199f452f8e88d4
gcouti <gcouti@gmail.com>
2017-09-10 18:18:20 -0300

    Changing how the execute the main flow

---
+++ source/agents/base.py
@@ -0,0 +1,193 @@
+import copy
+
+import keras
+callback = keras.callbacks.TensorBoard(log_dir='./log', histogram_freq=0, write_graph=True, write_images=True)
+
+
+import numpy as np
+from parlai.core.agents import Agent
+from parlai.core.dict import DictionaryAgent
+
+
+class BaseNeuralNetworkAgent(Agent):
+    """
+    Super class that can be used by all types of neural network agents.
+    """
+
+    @staticmethod
+    def add_cmdline_args(parser):
+        DictionaryAgent.add_cmdline_args(parser)
+
+        agent = parser.add_argument_group('Neural Networks Arguments')
+        agent.add_argument('-tm', '--text-max-size', type=int, default=30, help='Text maximum size')
+        agent.add_argument('-uc', '--use-candidates', type=bool, default=False, help='')
+
+    def __init__(self, opt):
+        super().__init__(opt)
+        self._dictionary = DictionaryAgent(opt)
+        self._episode_done = True
+        self._text_max_size = opt['text_max_size']
+        self._model_file = self.opt.get('model_file', None)
+        self._use_candidates = self.opt.get('use_candidates')
+
+    def _parse(self, texts):
+        if type(texts) == str:
+            return self._dictionary.txt2vec(texts)
+        else:
+            result = []
+            for t in texts:
+                result.append(self._dictionary.txt2vec(t)[0])
+            return result
+
+    def reset(self):
+        super().reset()
+        self._episode_done = True
+
+    def observe(self, obs):
+        observation = copy.deepcopy(obs)
+        if not self._episode_done:
+            # if the last example wasn't the end of an episode, then we need to
+            # recall what was said in that example
+            prev_dialogue = self.observation['text']
+            observation['text'] = prev_dialogue + '\n' + observation['text']
+        self.observation = observation
+        self._episode_done = observation['episode_done']
+        return observation
+
+    def act(self):
+        # call batch_act with this batch of one
+        return self.batch_act([self.observation])[0]
+
+    def batch_act(self, observations):
+
+        batchsize = len(observations)
+        # initialize a table of replies with this agent's id
+        batch_reply = [{'id': self.getID()} for _ in range(batchsize)]
+
+        # convert the observations into batches of inputs and targets
+        # valid_inds tells us the indices of all valid examples
+        # e.g. for input [{}, {'text': 'hello'}, {}, {}], valid_inds is [1]
+        # since the other three elements had no 'text' field
+        xs, qs, ys, cands = self.batchify(observations)
+
+        if xs is None:
+            # no valid examples, just return the empty responses we set up
+            return batch_reply
+
+        # produce predictions either way, but use the targets if available
+        predictions, cands = self.predict(xs, qs, ys, cands)
+
+        for i in range(len(predictions)):
+            # map the predictions back to non-empty examples in the batch
+            # we join with spaces since we produce tokens one at a time
+            curr = batch_reply[i]
+            curr['text_candidates'] = [self._dictionary.vec2txt([c]) for c in cands[i]]
+            if self._use_candidates:
+                i = np.argmax(predictions[i][cands[i]])
+                curr['text'] = curr['text_candidates'][i]
+            else:
+                curr['text'] = ' '.join(self._dictionary.vec2txt([np.argmax(predictions[i])]))
+
+            # batch_reply[i] = curr
+
+        return batch_reply
+
+    def _transform_input(self, tokenized, max_len=None):
+
+        if not max_len:
+            max_len = max([len(x) for x in tokenized])
+
+        result = []
+        for entry in tokenized:
+            new_x = [0] * max_len
+            new_x[:len(entry)] = entry[:max_len]
+            result.append(new_x)
+
+        return np.array(result)
+
+    def batchify(self, observations):
+        return None, None, None, None
+
+    def predict(self, xs, qs, ys, cands):
+        return ys, cands
+
+
+class BaseKerasAgent(BaseNeuralNetworkAgent):
+
+    @staticmethod
+    def add_cmdline_args(parser):
+        BaseNeuralNetworkAgent.add_cmdline_args(parser)
+
+        agent = parser.add_argument_group('Keras Arguments')
+        agent.add_argument('-kbs', '--keras-batch-size', type=int, default=1024, help='Batch size of keras')
+        agent.add_argument('-ke', '--keras-epochs', type=int, default=1000, help='Keras number of epochs')
+        agent.add_argument('-kefd', '--exclude-from-dict', type=int, default=5,
+                           help='Number of responses excluded from dictionary. This option get no effect when option '
+                                'use_candidates is True')
+
+    def __init__(self, opt):
+        super().__init__(opt)
+        self._model = None
+        self._epoch = opt['keras_epochs']
+        self._batch_size = opt['keras_batch_size']
+        self._exclude_from_dict = opt['exclude_from_dict']
+
+    def save(self, path=None):
+        """
+        Save model into a file
+
+        :param path: model file location, if it is none it will load the
+        """
+        path = self._model_file if path is None else path
+
+        if path:
+            model = {'opt': self.opt}
+            # TODO: save keras model into file
+            # with open(path, 'wb') as write:
+            #     torch.save(model, write)
+
+    @staticmethod
+    def load(path):
+        """Return opt and model states."""
+        model = None
+        with open(path, 'rb') as read:
+            print(read)
+            #TODO: load keras model
+
+        return model['opt'], model
+
+    """
+    This class is used can be extend with any kind of Keras agent
+    """
+    def batchify(self, observations):
+        """Convert a list of observations into input & target tensors."""
+        # valid examples
+        exs = [ex for ex in observations if 'text' in ex]
+
+        # tokenize the text
+        parsed = [self._parse('\n'.join(ex['text'].split('\n')[:-1])) for ex in exs]
+        xs = self._transform_input(parsed, max_len=self._text_max_size)
+
+        parsed = [self._parse('\n'.join(ex['text'].split('\n')[-1:])) for ex in exs]
+        qs = self._transform_input(parsed, max_len=self._text_max_size)
+
+        ys = None
+        cands = None
+
+        if 'labels' in exs[0]:
+            parsed = [self._parse(ex['labels']) for ex in exs]
+            ys = keras.utils.np_utils.to_categorical(self._transform_input(parsed), num_classes=len(self._dictionary))
+
+        if 'label_candidates' in exs[0]:
+            parsed = [self._parse(ex['label_candidates']) for ex in exs]
+            cands = self._transform_input(parsed)
+
+        return xs, qs, ys, cands,
+
+    def predict(self, xs, qs, ys=None, cands=None):
+        if ys is not None:
+            self._model.fit(xs, ys, verbose=True, batch_size=self._batch_size, epochs=self._epoch, callbacks=[callback])
+            return ys, cands
+        else:
+            predictions = self._model.predict(xs)
+            return predictions, cands
--- source/agents/my_agents.py
+++ source/agents/my_agents.py
@@ -1,46 +1,21 @@
-import copy
-
-from keras.backend import reshape
-from keras.legacy.layers import merge
-from keras.optimizers import SGD
-from parlai.core.dict import DictionaryAgent
+import numpy as np
 from parlai.core.agents import Agent
 
+from source.agents.base import BaseNeuralNetworkAgent, BaseKerasAgent
+
+from parlai.core.dict import DictionaryAgent
+
+from keras.optimizers import Adam
 from keras.models import Sequential, Model
-from keras.layers.embeddings import Embedding
-from keras.layers import Input, Activation, Dense, Permute, Dropout, add, dot, concatenate, BatchNormalization, \
-    Bidirectional, Merge, Reshape
-from keras.layers import LSTM, GRU
-
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Activation, Flatten, Input, Embedding, \
-    LSTM, Bidirectional, Lambda, Concatenate, Add, SimpleRNN
-from keras.layers.convolutional import Conv2D, MaxPooling2D, AveragePooling2D
-from keras.layers.normalization import BatchNormalization, regularizers
-from keras.optimizers import Adam, RMSprop
-from keras import backend as K
-
-import tensorflow as tf
-
-import collections
-import numpy as np
-import os
-
-from parlai.core.worlds import create_task
-from tensorflow.python.ops import rnn_cell_impl
-from tensorflow.python.ops.control_flow_ops import with_dependencies
-
-# import torch
-# import torch.nn as nn
-# import torch.nn.functional as F
-# import torch.optim as optim
-
-from torch.autograd import Variable
+from keras.layers import Permute, add, dot, concatenate, Reshape
+from keras.layers import Dense, Dropout, Activation, Flatten, Input, Embedding, LSTM, Lambda, Add, SimpleRNN
 
 
 class DummyAgent(Agent):
     """
-        This Agent retrieve only the first candidates
+        This agent retrieve only the first candidates.
+
+        This was made to simplify the understanding of the framework
     """
     @staticmethod
     def add_cmdline_args(parser):
@@ -67,106 +42,41 @@ class DummyAgent(Agent):
 
         # Rank candidates
         if 'label_candidates' in obs and len(obs['label_candidates']) > 0:
-            reply['text_candidates'] = self._rank_candidates(obs)
-            reply['text'] = reply['text_candidates'][0]
+            reply['text_candidates'] = list(obs['label_candidates'])
+            reply['text'] = np.random.choice(reply['text_candidates'], 1)[0]
         else:
             reply['text'] = "I don't know."
         return reply
 
-    @staticmethod
-    def _rank_candidates(obs):
-        return list(obs['label_candidates'])
-
-
-class BaseDictAgent(Agent):
+class RNNAgent(BaseKerasAgent):
 
     @staticmethod
     def add_cmdline_args(parser):
-        DictionaryAgent.add_cmdline_args(parser)
+        BaseKerasAgent.add_cmdline_args(parser)
 
-    def __init__(self, opt):
+    def __init__(self, opt, shared=None):
         super().__init__(opt)
-        self._dictionary, _ = self.create_dictionary(opt)
-        self._episode_done = True
+        self.id = 'RNNAgent'
+        self.opt = opt
 
-    @staticmethod
-    def create_dictionary(opt):
-        print('Setting up dictionary.')
+        # placeholders
+        input_sequence = Input(shape=(self._text_max_size, ))
+        # question = Input((128))
 
-        # set up dictionary
-        dictionary = DictionaryAgent(opt)
+        # # add the match matrix with the second input vector sequence
+        response = Dense(32, activation='relu')(input_sequence)
+        response = Dense(16, activation='relu')(response)
+        response = Dense(8, activation='relu')(response)
+        response = Dense(4, activation='relu')(response)
+        response = Dense(8, activation='relu')(response)
+        response = Dense(16, activation='relu')(response)
+        pred = Dense(len(self._dictionary), activation='softmax')(response)
 
-        if not opt.get('dict_loadpath'):
-            # build dictionary since we didn't load it
-            ordered_opt = copy.deepcopy(opt)
-            for datatype in ['train:ordered', 'valid']:
-                # we use train and valid sets to build dictionary
-                ordered_opt['datatype'] = datatype
-                ordered_opt['numthreads'] = 1
-                world_dict = create_task(ordered_opt, dictionary)
-
-                print('Dictionary building on {} data.'.format(datatype))
-                cnt = 0
-                # pass examples to dictionary
-                for _ in world_dict:
-                    cnt += 1
-                    if cnt > opt['dict_max_exs'] and opt['dict_max_exs'] > 0:
-                        print('Processed {} exs, moving on.'.format(
-                            opt['dict_max_exs']))
-                        # don't wait too long...
-                        break
-
-                    world_dict.parley()
-
-            # we need to save the dictionary to load it in memnn (sort it by freq)
-            dictionary.save(opt['dict_file'], sort=True)
-
-            print('Dictionary ready, moving on to training.')
-
-            return dictionary, opt
-
-    def save(self, fname):
-        self._dictionary.save(fname + '.dict')
-
-    def load(self, fname):
-        self._dictionary.load(fname + '.dict')
-
-    def reset(self):
-        super().reset()
-        self._episode_done = True
-
-    def observe(self, obs):
-        observation = copy.deepcopy(obs)
-        if not self._episode_done:
-            # if the last example wasn't the end of an episode, then we need to
-            # recall what was said in that example
-            prev_dialogue = self.observation['text']
-            observation['text'] = prev_dialogue + '\n' + observation['text']
-        self.observation = observation
-        self._episode_done = observation['episode_done']
-        return observation
-
-    def act(self):
-        if self.opt.get('datatype', '').startswith('train'):
-            self._dictionary.act()
-
-        obs = self.observation
-        reply = {}
-        reply['id'] = self.getID()
-
-        # Rank candidates
-        if 'label_candidates' in obs and len(obs['label_candidates']) > 0:
-            reply['text_candidates'] = self._rank_candidates(obs)
-            reply['text'] = reply['text_candidates'][0]
-        else:
-            reply['text'] = "I don't know."
-        return reply
-
-    def _rank_candidates(self, obs):
-        pass
+        self._model = Model(inputs=input_sequence, outputs=pred)
+        self._model.compile(optimizer="adadelta", loss='categorical_crossentropy', metrics=['accuracy'])
 
 
-class N2NMemAgent(BaseDictAgent):
+class N2NMemAgent(BaseKerasAgent):
     """
     Python + Keras implementation of End-to-end Memory Network
 
@@ -184,8 +94,19 @@ class N2NMemAgent(BaseDictAgent):
     """
     @staticmethod
     def add_cmdline_args(parser):
-        BaseDictAgent.add_cmdline_args(parser)
+        BaseNeuralNetworkAgent.add_cmdline_args(parser)
         parser.add_argument('-lp', '--length_penalty', default=0.5, help='length penalty for responses')
+
+        agent = parser.add_argument_group('N2NMem Arguments')
+
+        agent.add_argument('-hs', '--hiddensize', type=int, default=128, help='size of the hidden layers and embeddings')
+        agent.add_argument('-nl', '--numlayers', type=int, default=2, help='number of hidden layers')
+        agent.add_argument('-lr', '--learningrate', type=float, default=0.5, help='learning rate')
+        agent.add_argument('-dr', '--dropout', type=float, default=0.1, help='dropout rate')
+        agent.add_argument('-r', '--rank-candidates', type='bool', default=False,
+                           help='rank candidates if available. this is done by computing the' +
+                                ' mean score per token for each candidate and selecting the ' +
+                                'highest scoring one.')
 
     def __init__(self, opt, shared=None):
         super().__init__(opt)
@@ -199,38 +120,41 @@ class N2NMemAgent(BaseDictAgent):
 
         self._model = N2NMemory(self._vocab_size, self._story_maxlen, self._query_maxlen)
 
-    def _rank_candidates(self, obs):
+    def predict(self, xs, qs, ys=None, cands=None):
+        self._model.train(xs, xs, ys)
 
-        history = obs['text'].split('\n')
-        inputs_train = np.array([0] * self._story_maxlen)
-        history_vec = self._dictionary.txt2vec("\n".join(history[:len(history) - 1]))[:self._story_maxlen]
+    # def _rank_candidates(self, obs):
+    #
+    #     history = obs['text'].split('\n')
+    #     inputs_train = np.array([0] * self._story_maxlen)
+    #     history_vec = self._dictionary.txt2vec("\n".join(history[:len(history) - 1]))[:self._story_maxlen]
+    #
+    #     inputs_train[len(inputs_train) - len(history_vec):] = history_vec
+    #
+    #     queries_train = np.array([0] * self._query_maxlen)
+    #     query_vec = self._dictionary.txt2vec(history[len(history) - 1])[:self._query_maxlen]
+    #     queries_train[len(queries_train) - len(query_vec):] = query_vec
+    #
+    #     inputs_train = np.array([inputs_train])
+    #     queries_train = np.array([queries_train])
+    #
+    #     if 'labels' in obs:
+    #         answers_train = [0.] * self._vocab_size
+    #         answers_train[self._dictionary.txt2vec(obs['labels'][0])[0]] = 1
+    #
+    #         self._model.train(inputs_train, queries_train, np.array([answers_train]))
+    #
+    #         return [self._dictionary[np.argmax(answers_train)]]
+    #     else:
+    #         predicted = self._model.predict(inputs_train, queries_train)
+    #         return [self._dictionary[int(index)] for index in np.argsort(predicted[0])[::-1][:5]]
 
-        inputs_train[len(inputs_train) - len(history_vec):] = history_vec
 
-        queries_train = np.array([0] * self._query_maxlen)
-        query_vec = self._dictionary.txt2vec(history[len(history) - 1])[:self._query_maxlen]
-        queries_train[len(queries_train) - len(query_vec):] = query_vec
-
-        inputs_train = np.array([inputs_train])
-        queries_train = np.array([queries_train])
-
-        if 'labels' in obs:
-            answers_train = [0.] * self._vocab_size
-            answers_train[self._dictionary.txt2vec(obs['labels'][0])[0]] = 1
-
-            self._model.train(inputs_train, queries_train, np.array([answers_train]))
-
-            return [self._dictionary[np.argmax(answers_train)]]
-        else:
-            predicted = self._model.predict(inputs_train, queries_train)
-            return [self._dictionary[int(index)] for index in np.argsort(predicted[0])[::-1][:5]]
-
-
-class RNAgent(BaseDictAgent):
+class RNAgent(BaseNeuralNetworkAgent):
 
     @staticmethod
     def add_cmdline_args(parser):
-        BaseDictAgent.add_cmdline_args(parser)
+        BaseNeuralNetworkAgent.add_cmdline_args(parser)
 
     def __init__(self, opt, shared=None):
         super().__init__(opt)
@@ -255,6 +179,7 @@ class RNAgent(BaseDictAgent):
         history = obs['text'].split('\n')
         inputs_train = np.zeros((self._max_sentences, self._story_max_len))
         for story in history[:len(history) - 1][len(history) - self._max_sentences if len(history) > self._max_sentences else 0:]:
+
             history_vec = self._dictionary.txt2vec(story)[:self._story_max_len]
             inputs_train[iterator][:len(history_vec)] = history_vec
             iterator += 1
@@ -280,7 +205,7 @@ class RNAgent(BaseDictAgent):
             return [self._dictionary[int(index)] for index in np.argsort(predicted[0])[::-1][:5]]
 
 
-class EnsembleAgent(BaseDictAgent):
+class EnsembleAgent(BaseNeuralNetworkAgent):
 
     @staticmethod
     def add_cmdline_args(parser):
@@ -346,7 +271,7 @@ class EnsembleAgent(BaseDictAgent):
             return [self._dictionary[int(index)] for index in np.argsort(predicted[0])[::-1][:5]]
 
 
-class EnsembleNetworkAgent(BaseDictAgent):
+class EnsembleNetworkAgent(BaseNeuralNetworkAgent):
 
     @staticmethod
     def add_cmdline_args(parser):
@@ -403,7 +328,7 @@ class Networks:
         self._model = None
 
     def train(self, story, question, answer):
-        self._model.fit([story, question], answer, verbose=False, epochs=1)
+        self._model.fit([story, question], answer, verbose=True, epochs=1, callbacks=[callback])
 
     def predict(self, story, question):
         return self._model.predict([story, question], verbose=False)
@@ -437,15 +362,14 @@ class N2NMemory(Networks):
         self._vocab_size = vocab_size
 
         # placeholders
-        input_sequence = Input((self._story_maxlen,))
-        question = Input((self._query_maxlen,))
+        input_sequence = Input((None, 128))
+        question = Input((None, 128))
 
         answer = self.create_network(input_sequence, question)
 
         # build the final model
         self._model = Model([input_sequence, question], answer)
         self._model.compile(optimizer='rmsprop', loss='categorical_crossentropy', metrics=['accuracy'])
-
 
     def create_network(self, input_sequence, question):
 
@@ -456,6 +380,7 @@ class N2NMemory(Networks):
 
         # encoders
         # embed the input sequence into a sequence of vectors
+
         input_encoder_m = Sequential()
         input_encoder_m.add(Embedding(input_dim=self._vocab_size, output_dim=embedding))
         input_encoder_m.add(Dropout(drop_out))
@@ -500,6 +425,7 @@ class N2NMemory(Networks):
         answer = Dropout(drop_out)(answer)
         answer = Dense(self._vocab_size)(answer)  # (samples, vocab_size)
         # we output a probability distribution over the vocabulary
+
         return Activation(activation)(answer)
 
 
@@ -574,8 +500,6 @@ class RN(Networks):
         f_response = Dense(512, activation='relu')(f_response)
         f_response = Dropout(drop_out)(f_response)
         return Dense(self._vocab_size, activation='softmax')(f_response)
-
-
 
 
 class Ensemble(Networks):
