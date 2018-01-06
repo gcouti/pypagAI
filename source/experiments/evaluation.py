fc0d2909a00a197990e234a77b9dc11ffa28ce3e
gcouti <gcouti@gmail.com>
2018-01-06 09:55:11 -0200

    Experiments files

---
+++ source/experiments/evaluation.py
@@ -0,0 +1,52 @@
+from parlai.core.agents import create_agent
+from parlai.core.worlds import create_task
+from parlai.core.params import ParlaiParser
+from parlai.core.utils import Timer
+import build_dict
+import math
+
+
+def run_eval(agent, opt, datatype, max_exs=-1, write_log=False, valid_world=None):
+    """
+    Eval on validation/test data.
+
+    - Agent is the agent to use for the evaluation.
+    - opt is the options that specific the task, eval_task, etc
+    - datatype is the datatype to use, such as "valid" or "test"
+    - write_log specifies to write metrics to file if the model_file is set
+    - max_exs limits the number of examples if max_exs > 0
+    - valid_world can be an existing world which will be reset instead of reinitialized
+    """
+    print('[ running eval: ' + datatype + ' ]')
+    if 'stream' in opt['datatype']:
+        datatype += ':stream'
+    opt['datatype'] = datatype
+    if opt.get('evaltask'):
+        opt['task'] = opt['evaltask']
+
+    if valid_world is None:
+        valid_world = create_task(opt, agent)
+    else:
+        valid_world.reset()
+    cnt = 0
+    while not valid_world.epoch_done():
+        valid_world.parley()
+        if cnt == 0 and opt['display_examples']:
+            print(valid_world.display() + '\n~~')
+            print(valid_world.report())
+        cnt += opt['batchsize']
+        if 0 < max_exs <= cnt:
+            # note this max_exs is approximate--some batches won't always be
+            # full depending on the structure of the data
+            break
+    valid_report = valid_world.report()
+
+    metrics = datatype + ':' + str(valid_report)
+    print(metrics)
+    if write_log and opt['model_file']:
+        # Write out metrics
+        f = open(opt['model_file'] + '.' + datatype, 'a+')
+        f.write(metrics + '\n')
+        f.close()
+
+    return valid_report, valid_world
